7f15c51557d1a3f399e9f4bf4ea9f0da2f004b99
Mollerten <svedigt@gmail.com>
2022-05-05 10:17:19 +0200

    .

--- src/main/java/dat/carport/control/CreateUser.java
+++ src/main/java/dat/carport/control/CreateUser.java
@@ -1,25 +1,15 @@
-package dat.startcode.control;
+package dat.carport.control;
 
-import dat.startcode.model.config.ApplicationStart;
-import dat.startcode.model.entities.User;
-import dat.startcode.model.exceptions.DatabaseException;
-import dat.startcode.model.services.UserFacade;
-import dat.startcode.model.persistence.ConnectionPool;
+import dat.carport.model.exceptions.DatabaseException;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class CreateUser extends Command
 {
-    private ConnectionPool connectionPool;
-
     @Override
     String execute(HttpServletRequest request, HttpServletResponse response) throws DatabaseException
     {
-
         return "createUser";
     }
 }
