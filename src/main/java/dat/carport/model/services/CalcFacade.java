e9174b4e1e6e29d8e368e4b90ae647db572cbac9
Marcus <28458922+Altant457@users.noreply.github.com>
2022-05-12 11:31:19 +0200

    Add: Beams are calculated roughly

--- src/main/java/dat/carport/model/services/CalcFacade.java
+++ src/main/java/dat/carport/model/services/CalcFacade.java
@@ -1,13 +1,15 @@
 package dat.carport.model.services;
 
+import java.util.Map;
+
 public class CalcFacade {
 
     public static int calcPoles(int length, int width) {
         return Calculator.calcPoles(length, width);
     }
 
-    public static int calcBeams(int length, int width) {
-        return Calculator.calcRafters(length, width);
+    public static Map<Integer, Integer> calcBeams(int length, int width) {
+        return Calculator.calcBeams(length, width);
     }
 
 }
--- src/main/java/dat/carport/model/services/Calculator.java
+++ src/main/java/dat/carport/model/services/Calculator.java
@@ -1,8 +1,17 @@
 package dat.carport.model.services;
 
-class Calculator {
-    private static int[] beamLengths = {480, 510, 540, 570, 600};
+import java.util.HashMap;
+import java.util.Map;
 
+class Calculator {
+    private static int[] beamLengths = {300, 360, 420, 480, 540, 600}; //, 660, 720};
+
+    /**
+     *
+     * @param length The length of the carport
+     * @param width The width of the carport
+     * @return How many poles are needed for the given carport dimensions.
+     */
     // poles = stolper
     protected static int calcPoles(int length, int width) {
         int poleCount = 4;
@@ -12,13 +21,37 @@ class Calculator {
         return poleCount;
     }
 
-    // rafters = spær
-    protected static int calcRafters(int length, int width) {
-        return 0;
+    /**
+     *
+     * @param length The length of the carport
+     * @param width The width of the carport
+     * @return A {@code Map<Key, Value>} where keys are equal to the length of the beam
+     * and values are equal to the amount of beams with that length.
+     */
+    // beams = rem
+    protected static Map<Integer, Integer> calcBeams(int length, int width) {
+        Map<Integer, Integer> beams = new HashMap<>();
+        int beamLength;
+        int beamCount;
+        if (length > 410) {
+            beamLength = length / 2;
+            beamCount = 4;
+        } else {
+            beamLength = length;
+            beamCount = 2;
+        }
+        beams.put(beamLength, beamCount);
+        return beams;
     }
 
-    // beams = rem
-    protected static int calcBeams(int length, int width) {
-        return 0;
+    /**
+     *
+     * @param length The length of the carport
+     * @param width The width of the carport
+     * @return The amount of rafters needed for the given carport dimensions.
+     */
+    // rafters = spær
+    protected static int calcRafters(int length, int width) {
+        return 15;
     }
 }
