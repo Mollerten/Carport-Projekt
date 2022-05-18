c5c78d20e9a937adbebbe6d347d1e129e3711445
Marcus <28458922+Altant457@users.noreply.github.com>
2022-05-18 14:07:19 +0200

    More dynamic svg

--- src/main/java/dat/carport/control/SVGTest.java
+++ src/main/java/dat/carport/control/SVGTest.java
@@ -9,12 +9,47 @@ import javax.servlet.http.HttpServletResponse;
 public class SVGTest extends Command {
     @Override
     String execute(HttpServletRequest request, HttpServletResponse response) throws DatabaseException {
-        SVG svg = new SVG(0, 0, "0 0 855 690", 100, 100);
-        svg.addRect(10, 20, 100, 200);
-        svg.addRect(400, 100, 50, 200);
-        svg.addLine(0, 0, 100, 100, false);
-        svg.addLine(0, 50, 100, 150, true);
-        request.setAttribute("svgDrawing", svg.toString());
+        int rafterSpacing = 55;
+        int rafterCount = 15;
+        int rafterLength = 600;
+        int carportWidth = 600;
+        int carportLength = 780;
+        int dashLine_x1 = 0;
+        int dashLine_x2 = 0;
+        int dashLine_y1 = 37;
+        int dashLine_y2 = carportWidth- 38;
+        String viewBox = String.format("0 0 %d %d", carportLength, carportWidth);
+        SVG canvas = new SVG(0, 0, "0 0 800 800", 150, 150, true);
+        SVG carport = new SVG(0, 0, viewBox, 100, 100, false);
+
+        // Make the carport svg
+        // Add the poles
+        for (int i = 100; i < carportLength; i += 310) {
+            carport.addRect(i, 30, 15, 15);
+            carport.addRect(i, carportWidth-35-10, 15, 15);
+        }
+        // Add the beams
+        carport.addRect(0, 35, carportLength-10, 5.5f);
+        carport.addRect(0, carportWidth-35-5, carportLength-10, 5.5f);
+        // Add the rafters
+        for (int x = 0; x < rafterCount; x++) {
+            carport.addRect(rafterSpacing * x, 0, 4.5f, ((float) rafterLength));
+            if (x == 1) {
+                dashLine_x1 = rafterSpacing + 2;
+            }
+            if (x == ((int) Math.ceil(rafterCount * (2 / 3.0)))) {
+                dashLine_x2 = rafterSpacing * x + 2;
+            }
+        }
+        // Add the dashed lines
+        carport.addLine(dashLine_x1, dashLine_y1, dashLine_x2, dashLine_y2, true);
+        carport.addLine(dashLine_x2, dashLine_y1, dashLine_x1, dashLine_y2, true);
+
+        // Make the canvas SVG
+        canvas.addArrow(50, 50, 50, 500);
+        canvas.addSVG(carport);
+
+        request.setAttribute("svgDrawing", canvas.toString());
         return "svgtest";
     }
 }
--- src/main/java/dat/carport/model/services/SVG.java
+++ src/main/java/dat/carport/model/services/SVG.java
@@ -10,20 +10,47 @@ public class SVG {
     private int width;
     private int height;
 
-    private final String HEADER_TEMPLATE = "<svg height=\"%d%%\" width=\"%d%%\" viewBox=\"%s\" x=\"%s\" y=\"%s\" preserveAspectRatio=\"xMinYMin\">";
-    private final String RECT_TEMPLATE = "<rect x=\"%d\" y=\"%d\" height=\"%d\" width=\"%d\" style=\"stroke:#000;fill:#FFF\" />";
-    private final String LINE_TEMPLATE = "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke=\"black\"";
+    private final String ARROWDEFS = "<defs>\n" +
+            " <marker \n" +
+            " \tid=\"beginArrow\" \n" +
+            " \tmarkerWidth=\"12\" \n" +
+            " \tmarkerHeight=\"12\" \n" +
+            " \trefX=\"0\" \n" +
+            " \trefY=\"6\" \n" +
+            " \torient=\"auto\">\n" +
+            "   <path d=\"M0,6 L12,0 L12,12 L0,6\" style=\"fill: #000000;\" />\n" +
+            " </marker>\n" +
+            " <marker \n" +
+            " \tid=\"endArrow\" \n" +
+            " \tmarkerWidth=\"12\" \n" +
+            " \tmarkerHeight=\"12\" \n" +
+            " \trefX=\"12\" \n" +
+            " \trefY=\"6\" \n" +
+            " \torient=\"auto\">\n" +
+            "   <path d=\"M0,0 L12,6 L0,12 L0,0 \" style=\"fill: #000000;\" />\n" +
+            " </marker>\n" +
+            "</defs>";
 
-    public SVG(int x, int y, String viewBox, int width, int height) {
+    private final String HEADER_TEMPLATE = "<svg height=\"%d%%\" width=\"%d%%\" viewBox=\"%s\" x=\"%s\" y=\"%s\" preserveAspectRatio=\"xMinYMin\">";
+    private final String RECT_TEMPLATE = "<rect x=\"%d\" y=\"%d\" height=\"%f\" width=\"%f\" style=\"stroke:#000;fill:#FFF\" />";
+    private final String LINE_TEMPLATE = "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke=\"black\"";
+    private final String ARROW_TEMPLATE = "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" style=\"stroke: #000;" +
+            "marker-start: url(#beginArrow);" +
+            "marker-end: url(#endArrow);\" />";
+
+    public SVG(int x, int y, String viewBox, int width, int height, boolean arrows) {
         this.x = x;
         this.y = y;
         this.viewBox = viewBox;
         this.width = width;
         this.height = height;
         svg.append(String.format(HEADER_TEMPLATE, height, width, viewBox, x, y));
+        if (arrows) {
+            svg.append(ARROWDEFS);
+        }
     }
 
-    public void addRect(int x, int y, int height, int width) {
+    public void addRect(int x, int y, float width, float height) {
         svg.append(String.format(RECT_TEMPLATE, x, y, height, width));
     }
 
@@ -38,6 +65,10 @@ public class SVG {
 
     public void addSVG(SVG innerSVG) {
         svg.append(innerSVG.toString());
+    }
+
+    public void addArrow(int x1, int y1, int x2, int y2) {
+        svg.append(String.format(ARROW_TEMPLATE, x1, y1, x2, y2));
     }
 
     @Override
