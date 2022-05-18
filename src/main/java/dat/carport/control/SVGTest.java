008b8265f79536691e7a3f08dc8fc5b696b9b4f7
Marcus <28458922+Altant457@users.noreply.github.com>
2022-05-18 09:27:00 +0200

    Add: SVG drawing with dynamic header, lines and rect

--- src/main/java/dat/carport/control/SVGTest.java
+++ src/main/java/dat/carport/control/SVGTest.java
@@ -10,7 +10,10 @@ public class SVGTest extends Command {
     @Override
     String execute(HttpServletRequest request, HttpServletResponse response) throws DatabaseException {
         SVG svg = new SVG(0, 0, "0 0 855 690", 100, 100);
-        svg.addRect(10, 10, 100, 100);
+        svg.addRect(10, 20, 100, 200);
+        svg.addRect(400, 100, 50, 200);
+        svg.addLine(0, 0, 100, 100, false);
+        svg.addLine(0, 50, 100, 150, true);
         request.setAttribute("svgDrawing", svg.toString());
         return "svgtest";
     }
--- src/main/java/dat/carport/model/services/SVG.java
+++ src/main/java/dat/carport/model/services/SVG.java
@@ -12,6 +12,7 @@ public class SVG {
 
     private final String HEADER_TEMPLATE = "<svg height=\"%d%%\" width=\"%d%%\" viewBox=\"%s\" x=\"%s\" y=\"%s\" preserveAspectRatio=\"xMinYMin\">";
     private final String RECT_TEMPLATE = "<rect x=\"%d\" y=\"%d\" height=\"%d\" width=\"%d\" style=\"stroke:#000;fill:#FFF\" />";
+    private final String LINE_TEMPLATE = "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke=\"black\"";
 
     public SVG(int x, int y, String viewBox, int width, int height) {
         this.x = x;
@@ -24,6 +25,19 @@ public class SVG {
 
     public void addRect(int x, int y, int height, int width) {
         svg.append(String.format(RECT_TEMPLATE, x, y, height, width));
+    }
+
+    public void addLine(int x1, int y1, int x2, int y2, boolean dashedLines) {
+        svg.append(String.format(LINE_TEMPLATE, x1, y1, x2, y2));
+        if (dashedLines) {
+            svg.append(" stroke-dasharray=\"8\" />");
+        } else {
+            svg.append(" />");
+        }
+    }
+
+    public void addSVG(SVG innerSVG) {
+        svg.append(innerSVG.toString());
     }
 
     @Override
